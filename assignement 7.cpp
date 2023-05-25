52014485e53132020d730d420cde6a23564c3f4c
Zaara-khan <134476985+Zaara-khan@users.noreply.github.com>
2023-05-25 14:42:03 +0530

    Add files via upload

---
+++ assignement 7.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[]) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove the trailing newline character
+    if (str[strlen(str) - 1] == '\n') {
+        str[strlen(str) - 1] = '\0';
+    }
+
+    printf("Original string: %s\n", str);
+
+    reverseString(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+
